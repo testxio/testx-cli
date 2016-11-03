fab00862f711beab09673ce2ad188209c95345c2
Gantcho Kojuharov <gantcho@greyarch.net>
2016-11-03 16:32:06 +0100

    run a protractor test

--- bin/testx.js
+++ bin/testx.js
@@ -1,2 +1,17 @@
 #!/usr/bin/env node
-console.log('TESTX FTW!!!!');
+const spawn = require('child_process').spawn
+const cli = require('../lib')
+
+var program = require('commander');
+
+program
+  .version('0.1.0')
+  .arguments('<config_file>')
+  .description('Run a TestX project')
+  .action(function (config_file) {
+    spawn('/usr/local/lib/node_modules/testx-cli/node_modules/protractor/bin/protractor', [config_file], {stdio: 'inherit'});
+  });
+
+program.parse(process.argv);
+
+// TODO: Start a test
