179adc9bec740a69e999f0a7d6ed33c53fc45b96
Gantcho Kojuharov <gantcho@greyarch.net>
2017-05-13 17:17:55 +0200

    ask the user for project name, description and version

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const shell = require('shelljs')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 
 const copyTemplates = (tplsPath, templateName) => {
   let templatePath = path.join(tplsPath, templateName)
   if (fs.existsSync(templatePath)) {
-    console.log('Copying files…')
+    console.log('Copying files...')
     shell.cp('-R', `${templatePath}/*`, '.')
   } else {
     console.error(`The requested template "${templateName}" wasn\'t found.`)
@@ -13,26 +15,41 @@ const copyTemplates = (tplsPath, templateName) => {
   }
 }
 
-const replaceVars = () => {
-  shell.ls('-Rl', '.').forEach(entry => {
-    if (entry.isFile()) {
-      shell.sed('-i', '\\[MY_VAR\\]', '"testx"', entry.name)
-    }
+const replaceVars = vars => {
+  Object.entries(vars).forEach(([key, value]) => {
+    shell.sed('-i', `\\[${key.toUpperCase()}\\]`, value, 'package.json')
   })
 }
 
 const installDependencies = () => {
-  console.log('Installing dependencies…')
+  console.log('Installing dependencies...')
   shell.exec('npm i')
 }
 
+const questions = [
+  {
+    name: 'project',
+    message: 'Project name',
+    default: () => path.parse(process.cwd()).name
+  },
+  {
+    name: 'version',
+    message: 'Project version',
+    default: '0.0.1'
+  },
+  {
+    name: 'description',
+    message: 'Project description',
+    default: 'My awesome testx project'
+  }
+]
+
 const initTestxProject = (tplPath, localPath) => {
-  // Copy template files to the target dir
   copyTemplates(tplPath, localPath)
-  // TODO: Prompt the user for input (what input?)
-  // Go over all the template files and replace place holders with user input
-  replaceVars()
-  installDependencies()
+  return inquirer.prompt(questions).then(answers => {
+    replaceVars(answers)
+    installDependencies()
+  })
 }
 
 module.exports = opts => {
@@ -40,7 +57,10 @@ module.exports = opts => {
   let templatesPath = `${__dirname}/../templates`
   // let templateName = opts.coffee ? 'coffee' : 'js'
   let templateName = 'coffee'
-  initTestxProject(templatesPath, templateName)
-  console.log('Done!')
-  console.log('Use "npm run test-local" to execute the sample test script.')
+  initTestxProject(templatesPath, templateName).then(() => {
+    console.log(chalk.green('Done!'))
+    console.log(
+      `Use ${chalk.yellow('npm test')} to execute the sample test script.`
+    )
+  })
 }
